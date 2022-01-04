0e4832dcfeb9b4653a36a1afa89ef8c2cdc86024
randomnessyt92 <72773153+randomnessyt92@users.noreply.github.com>
2022-01-04 07:32:48 -0700

    Added WHY

---
+++ unused/why.py
@@ -0,0 +1,12 @@
+import time
+
+print('WHY')
+time.sleep(2)
+print('DO')
+time.sleep(2)
+print('YOU')
+time.sleep(1)
+print('HATE')
+time.sleep(.5)
+print('ME?')
+input('Press enter to not hate me and never be forgiven...')
