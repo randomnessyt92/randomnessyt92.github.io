8ebf159b10c889d16c4778f5a12037213a8d8b00
randomnessyt92 <randomnessyt92@gmail.com>
2024-03-01 00:01:37 -0700

    revamp

---
+++ yt/script.js
@@ -0,0 +1,8 @@
+function downloadVideo() {
+    var videoUrl = document.getElementById("videoUrl").value;
+    var downloadLinks = document.getElementById("downloadLinks");
+    downloadLinks.innerHTML = ""; // Clear previous download links
+
+    // Add logic to extract video ID and generate download links
+    // This depends on how you plan to implement the downloading process
+}
